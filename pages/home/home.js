3992e5224f79896a1d417f684f69355bc2945290
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-01-01 17:24:44 -0300

    mobile menu

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,8 +1,13 @@
+const body = document.querySelector('body');
+
 const Menu = {
   open(){
     document.querySelector('.mobile-menu').classList.add('active');
+    body.style.overflowY = 'hidden';
+
   },
   close(){
     document.querySelector('.mobile-menu').classList.remove('active');
+    body.style.overflowY = 'inherit';
   }
 }
